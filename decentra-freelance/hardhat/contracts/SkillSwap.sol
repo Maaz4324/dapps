5852454ce0898a6c50fac7b494efd2a9d7ab3f0b
Maaz4324 <90350901+Maaz4324@users.noreply.github.com>
2023-03-07 21:19:54 +0530

    chat system done offer option in progress

--- decentra-freelance/hardhat/contracts/SkillSwap.sol
+++ decentra-freelance/hardhat/contracts/SkillSwap.sol
@@ -8,9 +8,13 @@ contract SkillSwap{
          uint256 duration;
          address seller;
          address buyer;
+         bool inProgress;
      }
+
+     enum escrow {ordered, delivered, complete}
+
+     mapping(address=>mapping(address=>escrow)) public Transaction;
         
-    Deal public deal;
 
     struct Profile{
         address seller;
@@ -24,24 +28,6 @@ contract SkillSwap{
 
     mapping(address=>mapping(address=>Deal)) public dealSellrToBuyr;
 
-    function placeOrder(uint256 _amount, uint256 _duration, address _seller) public payable{
-        require(_seller != msg.sender, "you cannot order yourself");
-        require(_amount==msg.value, "Set the amount correctly");
-        deal = Deal(_amount, _duration + block.timestamp, _seller, msg.sender);
-        dealSellrToBuyr[_seller][msg.sender] = deal;
-     }
-
-    function backToBuyer(address _seller, address _buyer) public payable{
-        require(dealSellrToBuyr[_seller][_buyer].duration < block.timestamp);
-        payable(_buyer).transfer(dealSellrToBuyr[_seller][_buyer].amount);
-        delete dealSellrToBuyr[_seller][_buyer];
-    }
-
-    function toSeller(address _seller, address _buyer) public payable{
-        require(dealSellrToBuyr[_seller][_buyer].duration >= block.timestamp);
-        payable(_seller).transfer(dealSellrToBuyr[_seller][_buyer].amount - dealSellrToBuyr[_seller][_buyer].amount*1/10);
-        delete dealSellrToBuyr[_seller][_buyer];
-    }
 
     function setProfile(string memory _uri) public {
         require(isSeller[msg.sender]==false, "Already a seller");
@@ -57,6 +43,34 @@ contract SkillSwap{
         profile.uri = _uri;
     }
 
+    function placeOrder(uint256 _amount, uint256 _duration, address _seller) public payable{
+        require(isSeller[_seller]==true, "not a seller");
+        require(dealSellrToBuyr[_seller][msg.sender].inProgress == false, "already order in progress");
+        require(_seller != msg.sender, "you cannot order yourself");
+        // require(_amount+_amount*share/100==msg.value, "Set the amount correctly");
+        Transaction[_seller][msg.sender] = escrow.ordered;
+        dealSellrToBuyr[_seller][msg.sender] = Deal(_amount, _duration + block.timestamp, _seller, msg.sender, true);
+     }
+
+     function orderDelivered(address _seller) public {
+         require(Transaction[_seller][msg.sender] == escrow.ordered, "order not yet placed");
+         Transaction[_seller][msg.sender] = escrow.delivered;
+     }
+
+    function backToBuyer(address _seller, address _buyer) public payable{
+        require(dealSellrToBuyr[_seller][_buyer].duration < block.timestamp);
+        payable(_buyer).transfer(dealSellrToBuyr[_seller][_buyer].amount);
+        delete dealSellrToBuyr[_seller][_buyer];
+    }
+
+    function toSeller(address _seller, address _buyer) public payable{
+         require(Transaction[msg.sender][_buyer] == escrow.delivered, "order not yet delivered");
+        require(dealSellrToBuyr[_seller][_buyer].duration <= block.timestamp, "deadline is not met");
+        require(dealSellrToBuyr[msg.sender][_buyer].inProgress == true, "order is not made");
+         Transaction[_seller][msg.sender] = escrow.complete;
+        payable(_seller).transfer(dealSellrToBuyr[_seller][_buyer].amount);
+        delete dealSellrToBuyr[_seller][_buyer];
+    }
      function contractBalance() public view returns(uint256){
          return address(this).balance;
      }
