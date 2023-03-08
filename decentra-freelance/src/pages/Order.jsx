12ee1bb6a79da171717616d75285ad0590d1bfdb
Maaz Hamed <90350901+Maaz4324@users.noreply.github.com>
2023-03-08 19:39:54 +0530

    Update Order.jsx

--- decentra-freelance/src/pages/Order.jsx
+++ decentra-freelance/src/pages/Order.jsx
@@ -2,7 +2,9 @@ import styled from "styled-components";
 import { useEffect, useState } from "react";
 import SkillSwap from "../artifacts/contracts/SkillSwap.sol/SkillSwap.json";
 import { ethers } from "ethers";
-import { db } from "../firebase";
+import { db, storage } from "../firebase";
+// import firebase from "@firebase/app";
+
 import {
   collection,
   doc,
@@ -13,18 +15,26 @@ import {
   orderBy,
   onSnapshot,
   deleteDoc,
+  ref,
 } from "firebase/firestore";
+// import { ref } from "@firebase/firestore";
+import CountDown from "../component/CountDown";
+// import * as firebase from "firebase";
 
 function Order() {
   const [receiversAdd, setReceiversAdd] = useState([]);
   const [currentAcc, setCurrentAcc] = useState();
+  const [orderList, setOrderList] = useState([]);
+  const [sampleFile, setSampleFile] = useState();
+  const [originalFile, setOriginalFile] = useState();
+  const [percent, setPercent] = useState(0);
 
   const provider = new ethers.providers.Web3Provider(window.ethereum);
   const signer = provider.getSigner();
 
   const abi = SkillSwap.abi;
 
-  const contractAddress = "0xe7f1725E7734CE288F8367e1Bb143E90bb3F0512";
+  const contractAddress = "0x5FbDB2315678afecb367f032d93F642f64180aa3";
 
   const skillswap = new ethers.Contract(contractAddress, abi, signer);
 
@@ -49,11 +59,70 @@ function Order() {
     loadRecieverAdd();
   }, []);
 
+  function timeConverter(UNIX_timestamp) {
+    var a = new Date(UNIX_timestamp * 1000);
+    var months = [
+      "Jan",
+      "Feb",
+      "Mar",
+      "Apr",
+      "May",
+      "Jun",
+      "Jul",
+      "Aug",
+      "Sep",
+      "Oct",
+      "Nov",
+      "Dec",
+    ];
+    var year = a.getFullYear();
+    var month = months[a.getMonth()];
+    var date = a.getDate();
+    var hour = a.getHours();
+    var min = a.getMinutes();
+    var sec = a.getSeconds();
+    var time =
+      date + " " + month + " " + year + " " + hour + ":" + min + ":" + sec;
+    return time;
+  }
+
+  function handleUpload() {
+    if (!sampleFile || !originalFile) {
+      alert("Please choose a file first!");
+    }
+    try {
+      const storageRefSample = ref(storage, `/files/${sampleFile.name}`);
+      //   const storageRefOriginal = ref(storage, `/files/${originalFile.name}`);
+      const uploadSampleTask = uploadBytesResumable(
+        storageRefSample,
+        sampleFile
+      );
+      //   const uploadOriginalTask = uploadBytesResumable(storageRefOriginal, file);
+      uploadSampleTask.on(
+        "state_changed",
+        (snapshot) => {
+          const percent = Math.round(
+            (snapshot.bytesTransferred / snapshot.totalBytes) * 100
+          ); // update progress
+          setPercent(percent);
+        },
+        (err) => console.log(err),
+        () => {
+          // download url
+          getDownloadURL(uploadSampleTask.snapshot.ref).then((url) => {
+            console.log(url);
+          });
+        }
+      );
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
   useEffect(() => {
     async function loadOrders() {
       try {
         for (let i in receiversAdd) {
-          console.log(receiversAdd[i]);
           const orderAvailable = await skillswap.dealSellrToBuyr(
             currentAcc,
             receiversAdd[i]
@@ -62,9 +131,17 @@ function Order() {
             orderAvailable.seller !=
             "0x0000000000000000000000000000000000000000"
           ) {
+            let orderArray = [];
+            // let days = parseInt(orderAvailable.duration) / 86400;
+            let orderResult = {
+              amount: orderAvailable.amount.toString(),
+              deadline: orderAvailable.duration.toString(),
+            };
+            orderArray.push(orderResult);
+            setOrderList(orderArray);
             console.log(
               "🚀 ~ file: Order.jsx:56 ~ loadOrders ~ orderAvailable:",
-              orderAvailable
+              new Date().getDate()
             );
           }
         }
@@ -75,14 +152,87 @@ function Order() {
     loadOrders();
   }, [receiversAdd]);
 
-  return <Wrapper>Order</Wrapper>;
+  return (
+    <Wrapper>
+      <Container>
+        {orderList.map((orderData, idx) => (
+          <Delivery key={idx}>
+            <div>
+              <h5>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta
+                doloremque illum consequuntur suscipit aliquid! Iure enim ab
+                nemo nihil. Numquam voluptate doloribus minima quam, quasi
+                temporibus illo. Itaque, recusandae ipsum?
+              </h5>
+            </div>
+            <div>
+              <p>
+                <span>Time left</span>
+              </p>
+              <div className="timecolor">
+                <CountDown timeStamp={orderData.deadline * 1000} />
+              </div>
+            </div>
+            <div>
+              <input
+                type="file"
+                placeholder="Sample file"
+                onChange={(e) => setSampleFile(e.target.value)}
+              />
+              <input
+                type="file"
+                placeholder="Original work"
+                onChange={(e) => setOriginalFile(e.target.value)}
+              />
+              <button onClick={handleUpload}>Upload files</button>
+              <p>{percent} "% done"</p>
+            </div>
+            <div>
+              <p>
+                <span>Amount</span>
+              </p>
+              <h3>{orderData.amount}ETH</h3>
+            </div>
+          </Delivery>
+        ))}
+      </Container>
+    </Wrapper>
+  );
 }
 
 export default Order;
 
 const Wrapper = styled.div`
-  border: 3px solid green;
   min-height: 100vh;
   width: 100%;
   padding-top: 90px;
+  background: var(--black);
+`;
+
+const Container = styled.div`
+  width: 97%;
+  max-width: 1147px;
+  margin: 0 auto;
+`;
+
+const Delivery = styled.div`
+  padding: 50px 30px;
+  display: grid;
+  grid-template-columns: 50% 50%;
+  grid-gap: 30px;
+  align-items: center;
+  background: var(--darkBg);
+  border-radius: 10px;
+  h5 {
+    font-size: 24px;
+  }
+  h3 {
+    font-size: 28px;
+  }
+  span {
+    color: var(--darkText);
+  }
+  .timecolor {
+    color: var(--primary);
+  }
 `;
