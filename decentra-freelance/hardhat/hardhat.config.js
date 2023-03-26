b0646c7b663bc72e67bef111faae1ba7e384e31d
Maaz4324 <90350901+Maaz4324@users.noreply.github.com>
2023-03-27 03:06:35 +0530

    order page is left

--- decentra-freelance/hardhat/hardhat.config.js
+++ decentra-freelance/hardhat/hardhat.config.js
@@ -8,10 +8,8 @@ module.exports = {
   solidity: "0.8.17",
   networks: {
     goerli: {
-      url: "https://eth-goerli.g.alchemy.com/v2/HMem6t-3h4eeV-4eP5F6MCnKwIZqAEjH",
-      accounts: [
-        `249a384e7e19276680f54031177e39e4dc6584636e733a2847e2ff56b0701259`,
-      ],
+      url: "",
+      accounts: [``],
     },
   },
 };
